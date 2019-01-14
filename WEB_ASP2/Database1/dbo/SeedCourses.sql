9c5f7eca56a5cb999c2176dcfa86f985fa4b7b33
Damian Kreisich <kreisichd@gmail.com>
2019-01-14 01:10:59 +0100

    Seed fix

--- WEB_ASP2/Database1/dbo/SeedCourses.sql
+++ WEB_ASP2/Database1/dbo/SeedCourses.sql
@@ -1,7 +1,9 @@
 ﻿INSERT INTO kurs (Id, Name, Description, Photo, Cost)
 SELECT 0, 'Java od podstaw', 'Kurs javy dla każdego, tanio i szybko', 
 BulkColumn, 11.50
-FROM Openrowset(Bulk 'C:\Users\Master\source\repos\WEB_ASP2\WEB_ASP2\MediaFiles\java.png', Single_Blob) as img;
+FROM Openrowset
+(Bulk 'C:\Users\Master\source\repos\WEB_ASP2\WEB_ASP2\MediaFiles\java.png', Single_Blob)
+as photo;
 
 INSERT INTO kurs (Id, Name, Description, Photo, Cost)
 VALUES (1, 'C# od podstaw', 'Z nami ASP.NET nigdy nie będzie dla Ciebie skomplikowane', null, 9.12);
